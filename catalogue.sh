ab7935a6e379fbb2e8afbf75b335a8aa681f8641
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-07-26 20:27:12 +0530

    Roboshop script-Technical debt cleared

--- catalogue.sh
+++ catalogue.sh
@@ -1,19 +1,39 @@
+echo ">>>>>> Create Catalogue Service <<<<<<<<"
 cp catalogue.service /etc/systemd/system/catalogue.service
+
+echo ">>>>>> Create MongoDB Repo <<<<<<<<"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
+echo ">>>>>>> Install NodeJS Repos <<<<<<<<"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo ">>>>>>> Install NodeJS <<<<<<<<"
 yum install nodejs -y
+
+echo ">>>>>>> Create Application User  <<<<<<<<"
 useradd roboshop
+
+echo ">>>>>>> Create Application Directory <<<<<<<<"
 mkdir /app
+
+echo ">>>>>>> Download Application content <<<<<<<<"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
+echo ">>>>>>> Extract application content <<<<<<<<"
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
+
+echo ">>>>>>> Download NodeJS Dependencies <<<<<<<<"
 npm install
 
+echo ">>>>>>> Install Mongo Client <<<<<<<<"
 yum install mongodb-org-shell -y
+
+echo ">>>>>>> Load catalogue schema <<<<<<<<"
 mongo --host mongodb.maheshkoheda.online </app/schema/catalogue.js
 
+echo ">>>>>>> Start catalogue service <<<<<<<<"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
