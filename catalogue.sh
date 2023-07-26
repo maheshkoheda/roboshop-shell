dddb4818663b92a80ffd02d053148080e328b4c3
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-07-26 20:51:23 +0530

    Echo command

--- catalogue.sh
+++ catalogue.sh
@@ -1,39 +1,39 @@
-echo-e "\e[36m ">>>>>> Create Catalogue Service <<<<<<<<\e[0m"
+echo -e "\e[36m >>>>>> Create Catalogue Service <<<<<<<<\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service
 
-echo-e "\e[36m ">>>>>> Create MongoDB Repo <<<<<<<<\e[0m"
+echo -e "\e[36m >>>>>> Create MongoDB Repo <<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo-e "\e[36m ">>>>>>> Install NodeJS Repos <<<<<<<<\e[0m"
+echo -e "\e[36m >>>>>>> Install NodeJS Repos <<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo-e "\e[36m ">>>>>>> Install NodeJS <<<<<<<<\e[0m"
+echo -e "\e[36m >>>>>>> Install NodeJS <<<<<<<<\e[0m"
 yum install nodejs -y
 
-echo-e "\e[36m ">>>>>>> Create Application User  <<<<<<<<\e[0m"
+echo -e "\e[36m >>>>>>> Create Application User  <<<<<<<<\e[0m"
 useradd roboshop
 
-echo-e "\e[36m ">>>>>>> Create Application Directory <<<<<<<<\e[0m"
+echo -e "\e[36m >>>>>>> Create Application Directory <<<<<<<<\e[0m"
 mkdir /app
 
-echo-e "\e[36m ">>>>>>> Download Application content <<<<<<<<\e[0m"
+echo -e "\e[36m >>>>>>> Download Application content <<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 
-echo-e "\e[36m ">>>>>>> Extract application content <<<<<<<<\e[0m"
+echo -e "\e[36m >>>>>>> Extract application content <<<<<<<<\e[0m"
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
 
-echo-e "\e[36m ">>>>>>> Download NodeJS Dependencies <<<<<<<<\e[0m"
+echo -e "\e[36m >>>>>>> Download NodeJS Dependencies <<<<<<<<\e[0m"
 npm install
 
-echo-e "\e[36m ">>>>>>> Install Mongo Client <<<<<<<<\e[0m"
+echo -e "\e[36m >>>>>>> Install Mongo Client <<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
 
-echo-e "\e[36m ">>>>>>> Load catalogue schema <<<<<<<<\e[0m"
+echo -e "\e[36m >>>>>>> Load catalogue schema <<<<<<<<\e[0m"
 mongo --host mongodb.maheshkoheda.online </app/schema/catalogue.js
 
-echo-e "\e[36m ">>>>>>> Start catalogue service <<<<<<<<\e[0m"
+echo -e "\e[36m >>>>>>> Start catalogue service <<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
