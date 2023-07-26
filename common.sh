7bc6af06277201d9259153f3c76a4adf663e8f63
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-07-26 23:43:48 +0530

    Functions in roboshop

--- common.sh
+++ common.sh
@@ -1,4 +1,30 @@
-nodejs() {
+func_apppreq(){
+    echo -e "\e[36m >>>>>>> Create Application User  <<<<<<<<\e[0m"
+    useradd roboshop &>>${log}
+
+    echo -e "\e[36m >>>>>>> Cleanup Existing Application Content<<<<<<<<\e[0m"
+    rm -rf /app &>>${log}
+
+    echo -e "\e[36m >>>>>>> Create Application Directory <<<<<<<<\e[0m"
+    mkdir /app &>>${log}
+
+    echo -e "\e[36m >>>>>>> Download Application content <<<<<<<<\e[0m"
+    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+
+    echo -e "\e[36m >>>>>>> Extract application content <<<<<<<<\e[0m"
+    cd /app
+    unzip /tmp/${component}.zip &>>${log}
+    cd /app
+}
+func_systemd(){
+    echo -e "\e[36m >>>>>>> Start ${component} service <<<<<<<<\e[0m"
+    systemctl daemon-reload &>>${log}
+    systemctl enable ${component} &>>${log}
+    systemctl start ${component} &>>${log}
+}
+
+
+func_nodejs() {
   log=/tmp/roboshop.log
   echo -e "\e[36m >>>>>> Create Catalogue Service <<<<<<<<\e[0m"
   cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
@@ -12,22 +38,7 @@ nodejs() {
   echo -e "\e[36m >>>>>>> Install NodeJS <<<<<<<<\e[0m"
   yum install nodejs -y &>>${log}
 
-  echo -e "\e[36m >>>>>>> Create Application User  <<<<<<<<\e[0m"
-  useradd roboshop &>>${log}
-
-  echo -e "\e[36m >>>>>>> Create Application Directory <<<<<<<<\e[0m"
-  rm -rf /app &>>${log}
-
-  echo -e "\e[36m >>>>>>> Create Application Directory <<<<<<<<\e[0m"
-  mkdir /app &>>${log}
-
-  echo -e "\e[36m >>>>>>> Download Application content <<<<<<<<\e[0m"
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-
-  echo -e "\e[36m >>>>>>> Extract application content <<<<<<<<\e[0m"
-  cd /app
-  unzip /tmp/${component}.zip &>>${log}
-  cd /app
+  func_apppreq
 
   echo -e "\e[36m >>>>>>> Download NodeJS Dependencies <<<<<<<<\e[0m"
   npm install &>>${log}
@@ -38,11 +49,30 @@ nodejs() {
   echo -e "\e[36m >>>>>>> Load ${component} schema <<<<<<<<\e[0m"
   mongo --host mongodb.maheshkoheda.online </app/schema/${component}.js &>>${log}
 
-  echo -e "\e[36m >>>>>>> Start ${component} service <<<<<<<<\e[0m"
-  systemctl daemon-reload &>>${log}
-  systemctl enable ${component} &>>${log}
-  systemctl start ${component} &>>${log}
+   func_systemd
 
 
+
+}
+
+func_java() {
+ echo -e "\e[36m >>>>>>> Create ${component} service <<<<<<<<\e[0m"
+ cp ${component}.service /etc/systemd/system/${component}.service
+
+ echo -e "\e[36m >>>>>>> Install Maven <<<<<<<<\e[0m"
+ yum install maven -y
+
+ func_apppreq
+ echo -e "\e[36m >>>>>>> Build ${component} service <<<<<<<<\e[0m"
+ mvn clean package
+ mv target/${component}-1.0.jar ${component}.jar
+
+ echo -e "\e[36m >>>>>>> Install MySQL Client <<<<<<<<\e[0m"
+ yum install mysql -y
+
+ echo -e "\e[36m >>>>>>> Load Schema <<<<<<<<\e[0m"
+ mysql -h mysql.maheshkoheda.online -uroot -pRoboShop@1 < /app/schema/${component}.sql
+
+ func_systemd
 
 }
